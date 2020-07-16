947bf0842a0434fd2f9e7b59e21ffa6a67cc6891
Varun Naik <varun@naikvarun.org>
2020-07-15 19:07:16 -0500

    added some styling on auth page.

--- src/components/auth/AuthPage.js
+++ src/components/auth/AuthPage.js
@@ -1,47 +1,53 @@
 import React from 'react';
 import {AmplifyAuthenticator, AmplifySignIn, AmplifySignUp} from "@aws-amplify/ui-react";
+import {Row, Col} from "react-bootstrap";
 
 const AuthPage = () => {
     return (
-        <div>
-        <AmplifyAuthenticator>
-            <AmplifySignUp
-                slot="sign-up"
-                formFields={[
-                    {
-                        type: "username",
-                        label: "Username",
-                        placeholder: "username",
-                        required: true,
-                    },
-                    {
-                        type: "password",
-                        label: "Password",
-                        required: true,
-                    },
-                    {
-                        type: "email",
-                        label: "Email",
-                        placeholder: "john.doe@youremail.com",
-                        required: true,
-                    },
-                    {
-                        type: "phone_number",
-                        label: "Custom Phone Label",
-                        placeholder: "custom Phone placeholder",
-                        required: false,
-                    },
-                    {
-                        type: "name",
-                        label: "Name",
-                        placeholder: "John Doe",
-                        required: true,
-                    },
-                ]}
-            />
-            <AmplifySignIn slot="sign-in" />
-        </AmplifyAuthenticator>
-</div>
+        <div className="container-fluid">
+            <Row className="justify-content-md-center mt-5">
+                <Col md="auto">
+                    <AmplifyAuthenticator>
+                        <AmplifySignUp
+                            slot="sign-up"
+                            formFields={[
+                                {
+                                    type: "username",
+                                    label: "Username",
+                                    placeholder: "username",
+                                    required: true,
+                                },
+                                {
+                                    type: "password",
+                                    label: "Password",
+                                    required: true,
+                                },
+                                {
+                                    type: "email",
+                                    label: "Email",
+                                    placeholder: "john.doe@youremail.com",
+                                    required: true,
+                                },
+                                {
+                                    type: "phone_number",
+                                    label: "Custom Phone Label",
+                                    placeholder: "custom Phone placeholder",
+                                    required: false,
+                                },
+                                {
+                                    type: "name",
+                                    label: "Name",
+                                    placeholder: "John Doe",
+                                    required: true,
+                                },
+                            ]}
+                        />
+                        <AmplifySignIn headerText="Bitwise Rewards and Recognition" slot="sign-in"/>
+                    </AmplifyAuthenticator>
+                </Col>
+            </Row>
+
+        </div>
     )
 }
 export default AuthPage;
